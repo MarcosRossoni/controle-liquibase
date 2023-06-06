c63d366db117641fd1685b48073460892c94a951
marcosrossoni <marcosrossoni9@gmail.com>
2023-06-05 23:41:26 -0300

    criacao de sistema de controle liquibase

---
+++ src/main/java/com/br/controleliquibase/ControleLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.br.controleliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleLiquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/controleliquibase/ControleLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.controleliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
